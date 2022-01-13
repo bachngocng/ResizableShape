b125ea577de31275e3a62d9ed5ef9fc524591450
bachngocng <nnbach97@gmail.com>
2022-01-13 23:57:29 +0700

    done

---
+++ src/com/codegym/ResizableSquare.java
@@ -0,0 +1,20 @@
+package com.codegym;
+
+public class ResizableSquare extends Square implements Resizable {
+    public ResizableSquare() {
+    }
+
+    public ResizableSquare(double side) {
+        super(side);
+    }
+
+    public ResizableSquare(double side,String color, boolean filled) {
+        super(color, filled, side);
+    }
+
+    @Override
+    public void resize(double percent) {
+        double scale = percent / 100;
+        setSide(getSide() * scale);
+    }
+}
